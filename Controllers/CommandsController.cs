f526b9d87251563c603d4610b45f7c99434ad14e
grantsru <grantsru@gmail.com>
2020-08-21 18:44:34 -0400

    Apply responses to CommandsController

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Commander.Models;
+using Commander.Data;
 
 namespace Commander.Controllers
 {
@@ -7,6 +10,21 @@ namespace Commander.Controllers
   [ApiController]
   public class CommandsController: ControllerBase
   {
-    
+    private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+    // GET api/commands
+    [HttpGet]
+    public ActionResult <IEnumerable<Command>> GetAllCommands()
+    {
+      var commandItems = _repository.GetAllCommands();
+      return Ok(commandItems);
+    }
+
+    // GET api/commands/{id}
+    [HttpGet("{id}")]
+    public ActionResult <Command> GetCommandById(int id)
+    {
+      var commandItem = _repository.GetCommandById(id);
+      return Ok(commandItem);
+    }
   }
 }
